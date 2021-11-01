31dc912f0bc37c4344f5bf518efd3503b68d1ec8
Kokosik11 <lomjector98@gmail.com>
2021-11-01 21:38:50 +0300

    add heading style to App.css

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -12,10 +12,10 @@ const Main = props => {
           <div className={`${componentClass}_wrapper`}>
             <div className={`${componentClass}_content`}>
               <div className={`${componentClass}_content-text`}>
-                <h2>
+                <h1>
                   <span className="gradient-purple">{'5000 unique Axolotls'}</span>
                   <br/> {'that need owners'}
-                </h2>
+                </h1>
                 <span className={`${componentClass}_content_mainText`}>{'The Winged Axolotls is a NFT collection of 3333 unique Axolotls inspired by our Axoverse. Each axolotl has its own unique trait, and also belongs to one of 6 classes.'}</span>
               </div>
               <div className={`${componentClass}_gif`}>
